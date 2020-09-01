1bb20637be248cc80c138e947b71fcf53978c503
Javier Devoto <javierdevoto96@gmail.com>
2020-09-01 10:35:06 -0300

    Remuevo clases db y repos para pruebas funcionales

--- Backend/ShoppApi/src/main/java/com/challenge/repository/CartItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.challenge.repository;
-
-import com.challenge.dbobjects.CartItemDB;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CartItemRepository extends JpaRepository<CartItemDB, Long> {
-}
